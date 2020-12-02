8fbcbc4cd73b7927d251f6ac08d595dc05e4fdaa
Ian <ianlloydwilkinson@gmail.com>
2020-12-02 14:41:26 -0700

    initial commit, three functions done

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,12 +1,62 @@
 import {
   // methods,
   // methods,
-  originalArray
+  numberArray,
+  NAN,
+  originalArray,
+  doubleArray,
+  evensOnlyArray,
+  arraySum,
+  allNumbersGreaterThanZero,
 } from '../app'
+
 
 // hint of look in before_each before_all
 describe('originalArray Test', ()=> {
   test('originalArray should return it self', ()=>{
     expect(originalArray([1,2,3,4])).toEqual([1,2,3,4])
+  })
+})
+
+describe('doubleArray Tests', ()=> {
+  test('Array returns values with double values', () =>{
+    expect(doubleArray(numberArray)).toEqual([2,4,6,8])
+  })
+})
+
+describe('evensOnlyArray tests', () => {
+  test('Array returns only even values', () => {
+    expect(evensOnlyArray(numberArray)).toEqual([2,4])
+  })
+})
+
+describe('arraySum tests', () => {
+  test('returns sum of array values', () => {
+    expect(arraySum(numberArray)).toEqual(10)
+  })
+  test('If given strings, returns error', () => {
+    expect(arraySum(["Hello","World"])).toEqual(NAN)
+  })
+})
+
+describe('allNumbersGreaterThanZero tests', () => {
+  test('returns true or false for array', () => {
+    expect(allNumbersGreaterThanZero(numberArray)).toEqual(true)
+  })
+})
+
+
+
+
+
+
+
+
+
+
+
+describe('test names', () => {
+  test('specific test', () => {
+    
   })
 })
--- app.js
+++ app.js
@@ -1,3 +1,32 @@
+export const NAN = "NAN";
+export const numberArray=[1,2,3,4];
+
+
 export const originalArray = (arr) =>{
   return arr
+}
+//doubles values
+export const doubleArray = (numberArray) => {
+  return numberArray.map(value => value *2) 
+}
+
+export const evensOnlyArray = (numberArray) => {
+  let filteredArray = numberArray.filter(value => (value % 2 == 0));
+  return filteredArray;
+};
+
+export const arraySum = (numberArray) => {
+  const total = (total, nextNum) => total+nextNum;
+  let sum = numberArray.reduce(total, 0);
+  if (typeof sum === 'string') {
+    return NAN
+  } 
+  else {
+    return sum;
+  }
+};
+
+export const allNumbersGreaterThanZero = (numberArray) => {
+  const iseven = (value) => value >= 0 
+  return numberArray.every(iseven);
 }
